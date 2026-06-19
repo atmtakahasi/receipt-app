9fc3eac1fc0a20eb3574c7b8618613e2534b2c18
atmtakahasi <atmtakahasi@gmail.com>
2026-06-19 22:55:54 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ document.getElementById("readButton").addEventListener("click", async () => {
     const base64 = await toBase64(file);
 
     const response = await fetch(
-      "https://vision.googleapis.com/v1/images:annotate?key=YOUR_API_KEY",
+      "https://vision.googleapis.com/v1/images:annotate?key=AIzaSyB2VbazGuqD8k5U2zuy4GQaVTkmMVV0K-w",
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
